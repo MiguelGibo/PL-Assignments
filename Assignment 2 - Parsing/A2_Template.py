61bc4a83abd446e4528580754b4f9de3d088946a
MiguelGibo <miguel.giboyeaux@upr.edu>
2025-02-25 20:17:16 -0400

    Removed semantics and context, not needed for this assignment. Will be added back later.

--- Assignment 2 - Parsing/A2_Template.py
+++ Assignment 2 - Parsing/A2_Template.py
@@ -108,22 +108,18 @@ t_OR = r'\|'
 # global_facts -> facts exec_line 
 def p_global_facts(p):
   "global_facts : facts exec_line"
-  p[0] = ("program", p[1], p[2])
 
 # facts -> func_def facts 
 #  | assign   facts 
 #  | empty 
 def p_facts_func(p):
   "facts : func_def"
-  p[0] = [p[1]] + p[2]
 
 def p_facts_assign(p):
   "facts : assign facts"
-  p[0] = [p[1]] + p[2]
 
 def p_facts_empty(p):
    "facts : empty"
-   p[0] = []
 
  
 # func_def -> FUNC ID_FUNC LBRACE params RBRACE ASSIGN stm END
@@ -134,7 +130,7 @@ def p_facts_empty(p):
 #  | ID_FUNC 
 #  | ID
 def p_params(p):
-   "id"
+   pass
 
 # assign -> VAL ID ASSIGN stm END
 # TO-DO
@@ -169,13 +165,6 @@ def p_params(p):
 #  |LET facts IN stm END 
 def p_stm_number(p):
   'stm : NUMBER'
-  p[0] = ('number', p[1])
-
-precedence = (
-  ('left', 'PLUS', 'MINUS'),
-  ('left', 'TIMES', 'DIVIDE'),
-  ('right', 'UMINUS'),
-)
 
 def p_stm_binop(p):
   """stm : stm PLUS stm
@@ -188,50 +177,39 @@ def p_stm_binop(p):
         | stm EQUAL stm 
         | stm AND stm 
         | stm OR stm"""
-  p[0] = ('binop', p[2], p[1], p[3])
 
 def p_stm_uminus(p):
   "stm : MINUS stm %prec UMINUS"
-  p[0] = ('uminus', p[2])
 
 def p_stm_string(p):
   "stm : STRING"
-  p[0] = ('string', p[1])
 
 def p_stm_bool(p):
   """stm : TRUE
         | FALSE"""
-  p[0] = ('bool', p[1])
  
 def p_stm_nil(p):
   "stm : NIL"
-  p[0] = ('nil', p[1])
 
 def p_stm_identifier(p):
    "stm : IDENTIFIER"
-   p[0] = ("identifier", p[1])
 
 def p_stm_group(p):
    "stm : LPAREN stm RPAREN"
-   p[0] = p[2]
 
 def p_stm_if(p):
    "stm : IF stm THEN stm ELSE stm END"
-   p[0] = ('if_stm', p[2], p[4], p[6])
 
 def p_stm_let(p):
    "stm : LET facts IN stm END"
-   p[0] = ('let', p[2], p[4])
  
 # exec_line -> EXEC stm
 def p_exec_line(p):
    "exec_line : stm"
-   p[0] = ('exec', p[2])
 
 # empty lines
 def p_empty(p):
   'empty :'
-  p[0] = None
 
 # incorrect syntax
 def p_error(p):
